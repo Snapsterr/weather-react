ee65364b131d8eafe658082560d9b12a56ccb269
Snapsterr <vitaliy.chelyshev98@gmail.com>
2022-08-08 20:14:32 +0300

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -5,16 +5,10 @@ import { Provider } from "react-redux"
 import { store } from "./store/store"
 
 import App from "./App"
-import { useSelector } from "react-redux"
 
-// const isThemeDark = useSelector(
-//   (state) => state.switchParamsReducer.isThemeDark
-// )
 ReactDOM.render(
-  // <React.StrictMode>
   <Provider store={store}>
     <App />
   </Provider>,
-  // </React.StrictMode>,
   document.getElementById("root")
 )
